9652160273d0bed96e0a7f22c9eed5692c394e45
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 16:59:41 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,11 @@ def first_challenge
     }
   }
 
-  contacts["Freddy Mercury"]["favorite_icecream_flavors"]
-  flavors.delete_if {|elem| elem == "strawberry"}
-  #remember to return your newly altered contacts hash!
-  contacts
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute==:favorite_icecream_flavors
+        value.delete_if {|flavor| flavor=="strawberry"}
+
+      end
+    end 
 end
